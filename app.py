bdc5298a22ccc172f1785b24097ccbe85b507f9b
akshaycisco <sakskuma@cisco.com>
2016-11-15 23:55:07 -0700

    add reques requirement.txt

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def webhook():
 
 
 def processRequest(req):
-    return {}
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
     if req.get("result").get("action") == "yahooWeatherForecast":
         baseurl = "https://query.yahooapis.com/v1/public/yql?"
         yql_query = makeYqlQuery(req)
